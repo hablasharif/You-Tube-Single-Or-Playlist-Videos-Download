10bd4567a55fc7e57c81cb788b30b487271c84c3
hablasharif <138958393+hablasharif@users.noreply.github.com>
2023-12-05 22:04:25 +0600

    Update You-Tube-Single-Or-Playlist-Videos-Download.py

--- You-Tube-Single-Or-Playlist-Videos-Download.py
+++ You-Tube-Single-Or-Playlist-Videos-Download.py
@@ -3,41 +3,33 @@ import pytube
 import tqdm
 import os
 
-def download_youtube_video(video_url):
-    """Downloads a single YouTube video to the 'Hablu Progmmer javascript videos' folder.
+def download_youtube_video(video_url, download_path):
+    """Downloads a single YouTube video to the specified folder.
 
     Args:
         video_url: The URL of the YouTube video to download.
+        download_path: The path where the video will be saved.
     """
-    # Create the 'Hablu Progmmer javascript videos' folder if it doesn't exist
-    if not os.path.exists('Hablu Progmmer javascript videos'):
-        os.makedirs('Hablu Progmmer javascript videos')
-    
-    # Change the working directory to the 'Hablu Progmmer javascript videos' folder
-    os.chdir('Hablu Progmmer javascript videos')
-    
+    os.chdir(download_path)
     video = pytube.YouTube(video_url)
     video.streams.get_highest_resolution().download()
 
-def download_youtube_playlist(playlist_url):
-    """Downloads a YouTube playlist to the 'Hablu Progmmer javascript videos' folder.
+def download_youtube_playlist(playlist_url, download_path):
+    """Downloads a YouTube playlist to the specified folder.
 
     Args:
         playlist_url: The URL of the YouTube playlist to download.
+        download_path: The path where the playlist will be saved.
     """
-    # Create the 'Hablu Progmmer javascript videos' folder if it doesn't exist
-    if not os.path.exists('Hablu Progmmer javascript videos'):
-        os.makedirs('Hablu Progmmer javascript videos')
-    
-    # Change the working directory to the 'Hablu Progmmer javascript videos' folder
-    os.chdir('Hablu Progmmer javascript videos')
-    
+    os.chdir(download_path)
     playlist = pytube.Playlist(playlist_url)
     for video in tqdm.tqdm(playlist.videos):
         video.streams.get_highest_resolution().download()
 
 def main():
     st.title("YouTube Downloader")
+    drive = st.selectbox("Select Drive to Store:", options=['C:', 'D:', 'E:', 'F:', 'G:'])
+    download_path = os.path.join(drive, 'Hablu Progmmer javascript videos')
 
     choice = st.radio("Select an option:", ('Download a single video', 'Download a playlist', 'Show current path'))
 
@@ -45,7 +37,7 @@ def main():
         video_url = st.text_input("Enter the YouTube video URL:")
         if st.button("Download"):
             if video_url:
-                download_youtube_video(video_url)
+                download_youtube_video(video_url, download_path)
                 st.success("Video downloaded successfully!")
             else:
                 st.warning("Please enter a valid URL.")
@@ -53,13 +45,12 @@ def main():
         playlist_url = st.text_input("Enter the YouTube playlist URL:")
         if st.button("Download"):
             if playlist_url:
-                download_youtube_playlist(playlist_url)
+                download_youtube_playlist(playlist_url, download_path)
                 st.success("Playlist downloaded successfully!")
             else:
                 st.warning("Please enter a valid URL.")
     elif choice == 'Show current path':
-        current_path = os.getcwd()
-        st.write(f"The files are being saved in: {current_path}")
+        st.write(f"The files will be saved in: {download_path}")
 
 if __name__ == "__main__":
     main()
